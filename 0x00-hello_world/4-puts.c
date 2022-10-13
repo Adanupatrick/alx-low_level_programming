a94ff341da0579d0fc7ba77f909976247c62deed
Adanupatrick <abahpatrick02@gmail.com>
2022-10-13 16:03:54 -0700

    new C programme

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Printing new files alx
+ * Return: Always 0 (It a Success)
+ */
+init main(void)
+{
+	puts("\"Programming is like building multilingual puzzle");
+	return (0);
+}
