8f8a1e19b63c433cc22a85f98ed82e4a8868d418
Adanupatrick <abahpatrick02@gmail.com>
2022-10-21 13:53:56 -0700

    isdigit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,10 +4,10 @@
  * _isupper - uppercase letters
  * @c: char to check
  *
- * Return 0 or 1
+ * Return: 0 or 1
  */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
