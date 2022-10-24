5e08e77448ea2c9dacb96a055ead2d621492aec4
Adanupatrick <abahpatrick02@gmail.com>
2022-10-24 15:01:21 -0700

    new task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - printing files in reverse
+ * @s: string is always printed
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	int i - 0;
+
+	while (s[i] != '\0')
+		i++;
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
