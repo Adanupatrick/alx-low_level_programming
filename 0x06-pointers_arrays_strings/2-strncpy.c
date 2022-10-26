b8071e531f7ab4842be4d0f8fdb198c3571f5798
Adanupatrick <abahpatrick02@gmail.com>
2022-10-26 16:58:50 -0700

    new update

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - this functions copy files
+ * @dest: this copy to
+ * @src: this copy from
+ * @n: numbers to be copied
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
+
